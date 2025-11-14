28073da795c1003b0bb843f23deee6f0d4da663b
CriFigShe <crifigshe@gmail.com>
2025-11-14 15:04:04 +0100

    Context

---
+++ src/assets/assets.js
@@ -0,0 +1,7 @@
+import logo from "./inventario.png";
+import loginBg from "./pexels-polina-tankilevitch-4440789.jpg";
+
+export const assets = {
+    logo,
+    loginBg
+}
---
+++ src/pages/Register.jsx
@@ -0,0 +1,7 @@
+const Register = () => {
+    return(
+        <div>Signup</div>
+    )
+}
+
+export default Register;
